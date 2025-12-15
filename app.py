50af79db74c2fd3ff0752b1c8b34be2efcd154cf
Beth Cimini <bethac07@users.noreply.github.com>
2025-12-15 10:16:50 -0500

    Update schema URL post-refactor

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.session_state["created_pydantic"]=os.path.exists('bilayers_model.py')
 
 if not st.session_state["created_pydantic"]:
 
-    web_model_yaml = 'https://raw.githubusercontent.com/bilayer-containers/bilayers/refs/heads/main/tests/test_config/validate_schema.yaml' 
+    web_model_yaml = 'https://raw.githubusercontent.com/bilayer-containers/bilayers/refs/heads/main/src/bilayers/schema.yaml' 
     local_model_yaml = 'config.yaml'
 
     urllib.request.urlretrieve(web_model_yaml,local_model_yaml)
